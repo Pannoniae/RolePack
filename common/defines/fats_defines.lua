f1aa222140ca4324056410621cb0fb7dbf3a029b
Pannoniae <prisznyak.zsombor@gmail.com>
2021-03-04 12:29:18 +0000

    smol pp

--- common/defines/fats_defines.lua
+++ common/defines/fats_defines.lua
@@ -1,16 +1,16 @@
-NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_LOW = 3.0 -- thickness in pixels
-NDefines_Graphics.NGraphics.GRADIENT_BORDERS_COUNTRY_CENTER_THICKNESS = 1.0 -- The center gradient is linear 1/255 per pixel for this many pixels
-NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_HIGH = 20.0
-NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_STATE = 3.0
-NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_RESISTANCE = 3.0
-NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_INTEL_LEDGER = 3.0
-NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_SUPPLY_AREA_A = 3.0
-NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_SUPPLY_AREA_B = 10.0
-NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_STRATEGIC_REGIONS = 50.0
-NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_DIPLOMACY = 3.0
-NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_DIPLOMACY_ON_INTEL_LEDGER = 3.0
-NDefines_Graphics.NGraphics.COUNTRY_COLOR_SATURATION_MODIFIER = 0.8
-NDefines_Graphics.NGraphics.COUNTRY_COLOR_BRIGHTNESS_MODIFIER = 0.8
+--NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_LOW = 3.0 -- thickness in pixels
+--NDefines_Graphics.NGraphics.GRADIENT_BORDERS_COUNTRY_CENTER_THICKNESS = 1.0 -- The center gradient is linear 1/255 per pixel for this many pixels
+--NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_HIGH = 20.0
+--NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_STATE = 3.0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_RESISTANCE = 1.0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_INTEL_LEDGER = 1.0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_SUPPLY_AREA_A = 1.0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_SUPPLY_AREA_B = 1.0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_STRATEGIC_REGIONS = 1.0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_DIPLOMACY = 1.0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_DIPLOMACY_ON_INTEL_LEDGER = 1.0
+NDefines_Graphics.NGraphics.COUNTRY_COLOR_SATURATION_MODIFIER = 0.9
+NDefines_Graphics.NGraphics.COUNTRY_COLOR_BRIGHTNESS_MODIFIER = 0.9
 NDefines_Graphics.NMapMode.MAP_MODE_TERRAIN_TRANSPARENCY = 1.0
 NDefines_Graphics.NMapMode.MAP_MODE_NAVAL_TERRAIN_TRANSPARENCY = 1.0
 NDefines.NMilitary.BASE_COMBAT_WIDTH = 96                        -- base combat width
@@ -30,11 +30,33 @@ NDefines.NAir.AIR_DEPLOYMENT_DAYS = 0
 NDefines.NAir.AIR_WING_FLIGHT_SPEED_MULT = 0.2
 
 NDefines.NCountry.EVENT_PROCESS_OFFSET = 31 --20 
-NDefines.NGame.GAME_SPEED_SECONDS = { 1.0, 0.25, 0.1, 0.05, 0.0 } --{2.0, 0.5, 0.2, 0.1, 0.0}
+NDefines.NGame.GAME_SPEED_SECONDS = { 1.0, 0.1, 0.5, 0.025, 0.0 } --{2.0, 0.5, 0.2, 0.1, 0.0}
 
 NDefines_Graphics.NGraphics.CAPITAL_ICON_CUTOFF = 800						-- 1100 
 NDefines_Graphics.NGraphics.DRAW_MAP_OBJECTS_CUTOFF = 250 					-- 550 
 NDefines_Graphics.NGraphics.MAP_BUILDINGS_SHRINK_DISTANCE = 100				-- 180
+NDefines_Graphics.NGraphics.BORDER_WIDTH = 1.0
+NDefines_Graphics.NGraphics.DRAW_SHADOWS_CUTOFF = 800
+NDefines_Graphics.NGraphics.DRAW_SHADOWS_FADE_LENGTH = 100
+NDefines_Graphics.NGraphics.DRAW_FOW_CUTOFF = 108
+NDefines_Graphics.NGraphics.DRAW_FOW_FADE_LENGTH = 10
+NDefines_Graphics.NGraphics.WEATHER_DISTANCE_CUTOFF = 50 -- At what distance weather effects are hidden
+NDefines_Graphics.NGraphics.WEATHER_DISTANCE_FADE_LENGTH = 400 -- How far the fade out distance should be
+NDefines_Graphics.NGraphics.CAMERA_OUTSIDE_MAP_DISTANCE_TOP = 100.0
+NDefines_Graphics.NGraphics.CAMERA_OUTSIDE_MAP_DISTANCE_BOTTOM = 50.0
+NDefines_Graphics.NGraphics.CAMERA_ZOOM_SPEED_DISTANCE_MULT = 10.0
+NDefines_Graphics.NGraphics.CAMERA_ZOOM_KEY_SCALE = 0.10
+NDefines_Graphics.NGraphics.DRAW_REFRACTIONS_CUTOFF = 300
+NDefines_Graphics.NGraphics.DRAW_DETAILED_CUTOFF = 500		-- When country names disappear from the map
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_OUTLINE_CUTOFF_COUNTRY = 0.98
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_OUTLINE_CUTOFF_DIPLOMACY = 0.98
+NDefines_Graphics.NFrontend.CAMERA_MIN_HEIGHT = 100.0
+NDefines_Graphics.NMapMode.SELECTED_COUNTRY_HIGHLIGHT_THICKNESS_MULT = 1.0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_FIELD_COUNTRY_LOW = 0.0 -- country area in sum of pixels ...
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_FIELD_COUNTRY_HIGH = 9000.0 -- ... the value is squared, so fe. country of size 100x100pix = 10000
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_LOW = 0.0 -- thickness in pixels
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_HIGH = 35.0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_STATE = 1.0
 
 NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 3 --12 Y E E T
 
@@ -45,4 +67,4 @@ NDefines.NProduction.ANNEX_CONVOYS_RATIO = 0.75
 NDefines.NProduction.CAPITULATE_STOCKPILES_RATIO = 0.75
 NDefines.NProduction.CAPITULATE_FUEL_RATIO = 0.75
 
-NDefines.NPolitics.BASE_POLITICAL_POWER_INCREASE = 3
+NDefines.NPolitics.BASE_POLITICAL_POWER_INCREASE = 2.5
